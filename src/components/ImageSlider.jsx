6d1c2e22222c8e3517174df486590bef94301033
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-03-24 23:49:44 +0300

    add timer

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,74 +1,83 @@
-import { useState } from 'react'
-import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa'
-import '../index.css'
-
+import { useState, useEffect, useRef } from 'react';
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
+import '../index.css';
 
 export default function ImageSlider({ slides }) {
+  const [current, setCurrent] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const length = slides.length;
 
-    console.log('slides', slides)
+  const goToNext = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+    setActiveIndex(current === length - 1 ? 0 : current + 1);
+    resetTimer();
+  };
 
-    const [current, setCurrent] = useState(0)
-    const [activeIndex, setActiveIndex] = useState(0)
-    const length = slides.length
+  const goToPrevious = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+    setActiveIndex(current === 0 ? length - 1 : current - 1);
+    resetTimer();
+  };
 
-    const goToNext = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1)
-        setActiveIndex(current === length - 1 ? 0 : current + 1)
-    }
+  const resetTimer = () => {
+    clearInterval(timer.current);
+    timer.current = setInterval(goToNext, 3000);
+  };
 
-    const goToPrevious = () => {
-        setCurrent(current === 0 ? length - 1 : current - 1)
-        setActiveIndex(current === 0 ? length - 1 : current - 1)
-    }
+  const styleImage = {
+    backgroundImage: `url(${slides[current]})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    borderRadius: '10px',
+    height: '60vh',
+    width: '100%',
+    margin: '0 auto',
+  };
 
-    const styleImage = {
-        backgroundImage: `url(${slides[current]})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        borderRadius: '10px',
-        height: '60vh',
-        width: '50%',
-        margin: '0 auto',
-    }
+  const dotStyle = {
+    height: '20px',
+    width: '20px',
+    backgroundColor: '#bbb',
+    borderRadius: '50%',
+    display: 'inline-block',
+    margin: '0 2px',
+    cursor: 'pointer',
+  };
 
-    const dotStyle = {
-        height: '20px',
-        width: '20px',
-        backgroundColor: '#bbb',
-        borderRadius: '50%',
-        display: 'inline-block',
-        margin: '0 2px',
-        cursor: 'pointer',
+  const dots = slides.map((_, index) => (
+    <div
+      key={index}
+      style={{ ...dotStyle, backgroundColor: activeIndex === index ? 'pink' : '#bbb' }}
+      onClick={() => {
+        setActiveIndex(index);
+        setCurrent(index);
+        resetTimer();
+      }}
+    />
+  ));
 
-    }
+  const timer = useRef(null);
 
-    const dots = slides.map((_, index) => (
-        <div
-          key={index}
-          style={{ ...dotStyle, backgroundColor: activeIndex === index ? '#717171' : '#bbb' }}
-          onClick={() => {
-            setActiveIndex(index);
-            setCurrent(index);
-          }}
-        />
-      ));
+  useEffect(() => {
+    timer.current = setInterval(goToNext, 3000);
+    return () => clearInterval(timer.current);
+  }, [current]);
 
-    return(
-        <>
-            <div className="slider">
-                <div className='leftArrow' onClick={goToPrevious}>
-                <FaArrowCircleLeft size={30}/>                
-                </div>
+  return (
+    <>
+      <div className="slider">
+        <div className="leftArrow" onClick={goToPrevious}>
+          <FaArrowCircleLeft size={30} />
+        </div>
 
-                <div className='rightArrow' onClick={goToNext}>
-                <FaArrowCircleRight size={30} />                    
-                </div>
+        <div className="rightArrow" onClick={goToNext}>
+          <FaArrowCircleRight size={30} />
+        </div>
 
-                <div style={styleImage}></div>
-                <div style={{ textAlign: 'center', marginTop: '10px' }}>{dots}</div>
-
-            </div>
-        </>
-    )
-}  
+        <div style={styleImage}></div>
+        <div className='dots__wrapper' style={{ textAlign: 'center', marginTop: '10px' }}>{dots}</div>
+      </div>
+    </>
+  );
+}
